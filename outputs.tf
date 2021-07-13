df24959f8468bfbf7cdff949e715e9c012c91324
Jahn Saito <jahn.saito@gmail.com>
2021-07-13 17:50:17 +0200

    Output name of the Rollbar project

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
+output "rollbar_project_name" {
+  value = data.rollbar_project.this.name
+
+  description = "The name of the Rollbar project."
+}
+
 output "secretsmanager_secret" {
   value = aws_secretsmanager_secret.this
 
