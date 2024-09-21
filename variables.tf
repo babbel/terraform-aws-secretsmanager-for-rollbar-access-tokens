c4e478c364da8063c970485bf50b3119212a98a1
Jan Sebastian Siwy <github@jansiwy.de>
2024-09-21 19:58:23 +0200

    Add support for resource-specific tags

--- variables.tf
+++ variables.tf
@@ -1,14 +1,18 @@
-variable "name_prefix" {
-  type = string
-
-  description = "Name prefix for the SecretsManager. The full name will be $${var.name_prefix}.rollbar_access_tokens."
-}
-
-variable "tags" {
+variable "default_tags" {
   type    = map(string)
   default = {}
 
-  description = "Tags which will be assigned to all resources."
+  description = <<EOS
+Map of tags assigned to all AWS resources created by this module.
+EOS
+}
+
+variable "name_prefix" {
+  type = string
+
+  description = <<EOS
+Name prefix for the SecretsManager. The full name will be $${var.name_prefix}.rollbar_access_tokens.
+EOS
 }
 
 variable "rollbar_tokens" {
@@ -17,5 +21,16 @@ variable "rollbar_tokens" {
     access_token = string
   }))
 
-  description = "List of objects having access tokens names and the token values which shall be loaded into the SecretsManager."
+  description = <<EOS
+List of objects having access tokens names and the token values which shall be loaded into the SecretsManager.
+EOS
+}
+
+variable "secretsmanager_secret_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the SecretsManager secret.
+EOS
 }
